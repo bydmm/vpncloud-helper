4e0ecb1ea4f855b56f82e364c0009943809393be
bydmm <521194743@qq.com>
2014-05-09 11:24:18 +0800

    rebuild

--- ping.rb
+++ ping.rb
@@ -1,38 +1,48 @@
 require 'net/ping'
 require 'colorize'
 
+# No more Talk and **** RuboCop
 class VpnTest
   def test
     domains.each do |domain|
       ping(domain)
     end
+    puts 'Which VPN would you like?'
+    vpn_name = gets.strip
+    puts "Connecting to #{vpn_name}......"
+    ip = SwitchIp.new
+    ip.go vpn_name
   end
 
   private
 
   def ping(url)
-    sum = 0
-    bugs = 0
+    result = connect(url)
+    sum, bugs = result[:sum], result[:bugs]
+    outputs = "ping #{url} : #{sum}ms, failed: #{bugs}.times"
+    color = bug_color(bugs)
+    puts outputs.colorize(color)
+  end
+
+  def bug_color(bugs)
+    if bugs == 0 then :green
+    elsif bugs > 0 && bugs < 5 then :yellow
+    elsif bugs == 5 then :red
+    end
+  end
+
+  def connect(url)
+    sum, bugs = 0, 0
     5.times do
       from = Time.now
       pt = Net::Ping::External.new(url)
       if pt.ping
         sum += (Time.now - from) * 1000
       else
-        puts "ping #{url}: #{pt.exception}".colorize(:yellow)
         bugs += 1
       end
     end
-    outputs = "ping #{url} : #{(sum / 5).round}ms, failed: #{bugs}.times"
-    color =
-      if bugs == 0
-        :green
-      elsif bugs > 1 && bugs < 5
-        :yellow
-      elsif bugs == 5
-        :red
-      end
-    puts outputs.colorize(color)
+    { sum: (sum / 5).round, bugs: bugs }
   end
 
   def domains
@@ -43,42 +53,38 @@ class VpnTest
       end
     vpns << 'www.baidu.com'
   end
-end
 
-class SwitchIp
-  def go(vpn_name)
-    turn_off vpn_name
-    sleep 3
-    turn_on vpn_name
-  end
+  # No more Talk and **** RuboCop
+  class SwitchIp
+    def go(vpn_name)
+      turn_off vpn_name
+      sleep 3
+      turn_on vpn_name
+    end
 
-  def turn_on(vpn_name)
-    system "/usr/bin/env osascript <<-EOF
+    def turn_on(vpn_name)
+      system "/usr/bin/env osascript <<-EOF
+          tell application \"System Events\"
+            tell current location of network preferences
+                set VPN to service \"#{vpn_name}\" -- your VPN name here
+                if exists VPN then connect VPN
+          end tell
+        end tell
+      EOF"
+    end
+
+    def turn_off(vpn_name)
+      system "/usr/bin/env osascript <<-EOF
         tell application \"System Events\"
           tell current location of network preferences
-              set VPN to service \"#{vpn_name}\" -- your VPN name here
-              if exists VPN then connect VPN
-        end tell
+                set VPN to service \"#{vpn_name}\" -- your VPN name here
+                if exists VPN then disconnect VPN
+          end tell
       end tell
-    EOF"
-  end
-
-  def turn_off(vpn_name)
-    system "/usr/bin/env osascript <<-EOF
-      tell application \"System Events\"
-        tell current location of network preferences
-              set VPN to service \"#{vpn_name}\" -- your VPN name here
-              if exists VPN then disconnect VPN
-        end tell
-    end tell
-   EOF"
+     EOF"
+    end
   end
 end
 
 vpn = VpnTest.new
 vpn.test
-puts 'Which VPN would you like?'
-vpn_name = gets.strip
-puts "Connecting to #{vpn_name}......"
-ip = SwitchIp.new
-ip.go vpn_name
